bf7943daab0879354a0101cf1c2fa35f1c10d224
Anita <sharma.anita00001@gmail.com>
2023-03-28 20:31:31 +0545

    Delete boilerplate from Create React App

--- src/components/CalculatorButton.js
+++ src/components/CalculatorButton.js
@@ -1,14 +1,12 @@
 import './CalculatorButton.css';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 
-function CalculatorButton({ label, onClick }) {
+function CalculatorButton({ label }) {
   const isWide = label === '0';
   return (
     <button
       type="button"
       className={`calculator-button ${isWide ? 'wide' : ''}`}
-      onClick={onClick}
     >
       {label}
     </button>
@@ -17,7 +15,6 @@ function CalculatorButton({ label, onClick }) {
 
 CalculatorButton.propTypes = {
   label: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
 
 export default CalculatorButton;
