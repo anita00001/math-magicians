7de32703e74354da83201e2a4c221b963592ea9c
Anita <sharma.anita00001@gmail.com>
2023-03-29 14:54:47 +0545

    Add click event on calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,50 @@
+import React, { useState } from 'react';
 import CalculatorButton from './CalculatorButton';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [displayValue, setDisplayValue] = useState('0');
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    const newData = calculate(calculatorData, buttonName);
+    setCalculatorData(newData);
+
+    // Update the display value
+    const newDisplayValue = newData.next || newData.total || '0';
+    setDisplayValue(newDisplayValue);
+  };
+
+  const renderButton = (label) => (
+    <CalculatorButton
+      key={label}
+      label={label}
+      onClick={() => handleClick(label)}
+    />
+  );
+
   return (
     <div className="calculator">
-      <div className="calculator-output">0</div>
-      <div className="calculator-row">
-        <CalculatorButton label="AC" />
-        <CalculatorButton label="+/-" />
-        <CalculatorButton label="%" />
-        <CalculatorButton label="/" />
+      <div className="calculator-output">{displayValue}</div>
+      <div className="calculator-panel">
+        {['AC', '+/-', '%', '÷'].map((label) => renderButton(label))}
       </div>
-      <div className="calculator-row">
-        <CalculatorButton label="7" />
-        <CalculatorButton label="8" />
-        <CalculatorButton label="9" />
-        <CalculatorButton label="*" />
+      <div className="calculator-panel">
+        {['7', '8', '9', 'x'].map((label) => renderButton(label))}
       </div>
-      <div className="calculator-row">
-        <CalculatorButton label="4" />
-        <CalculatorButton label="5" />
-        <CalculatorButton label="6" />
-        <CalculatorButton label="-" />
+      <div className="calculator-panel">
+        {['4', '5', '6', '-'].map((label) => renderButton(label))}
       </div>
-      <div className="calculator-row">
-        <CalculatorButton label="1" />
-        <CalculatorButton label="2" />
-        <CalculatorButton label="3" />
-        <CalculatorButton label="+" />
+      <div className="calculator-panel">
+        {['1', '2', '3', '+'].map((label) => renderButton(label))}
       </div>
-      <div className="calculator-row calculator-row-last">
-        <CalculatorButton label="0" />
-        <CalculatorButton label="." />
-        <CalculatorButton label="=" />
+      <div className="calculator-panel">
+        {['0', '.', '='].map((label) => renderButton(label))}
       </div>
     </div>
   );
--- src/components/CalculatorButton.js
+++ src/components/CalculatorButton.js
@@ -1,12 +1,13 @@
 import './CalculatorButton.css';
 import PropTypes from 'prop-types';
 
-function CalculatorButton({ label }) {
+function CalculatorButton({ label, onClick }) {
   const isWide = label === '0';
   return (
     <button
       type="button"
       className={`calculator-button ${isWide ? 'wide' : ''}`}
+      onClick={onClick}
     >
       {label}
     </button>
@@ -15,6 +16,7 @@ function CalculatorButton({ label }) {
 
 CalculatorButton.propTypes = {
   label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default CalculatorButton;
